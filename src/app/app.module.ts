67affca84d460ad4999408d169c4b02723baff0a
SaeedShah1 <saeedshah1901@gmail.com>
2021-09-30 16:34:13 +0500

    New Login Page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,17 @@ import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import {FormsModule} from '@angular/forms';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from "@angular/material/button";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
@@ -14,12 +25,22 @@ import {FormsModule} from '@angular/forms';
     EmployeeComponent,
     HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    FlexLayoutModule,
+    MatSliderModule,
+    MatCardModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatInputModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
